dc66c8e23371b79c8501c382f2a02dd053f2d24a
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2022-09-29 02:54:43 +0600

    added all type of activities

--- src/Components/Activities/Activities.js
+++ src/Components/Activities/Activities.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Exercises from '../Exercises/Exercises'
 import './Activities.css'
 const Activities = () => {
+    const [activities,setActivities] = useState([])
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setActivities(data))
+    },[])
   return (
     <div className='activity-container'>
-        <div>
-            Activities
+        <div className='exercises'>
+            {
+                activities.map(activity => <Exercises activity = {activity}/>)
+            }
+         
         </div>
         <div>
             calculation
---
+++ src/Components/Exercises/Exercises.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Exercises.css'
+
+const Exercises = (props) => {
+    const {id,img,name,age,time} = props.activity
+  return (
+    <div className='exercise'>
+        <img src={img} alt="" />
+        <div className='exercise-details'>
+            <p>Name:{name}</p>
+            <p>For Age: {age}</p>
+            <p>Time Required: {time}m</p>
+            <button className='cart-btn'>Add to Cart</button>
+        </div>
+    </div>
+  )
+}
+
+export default Exercises
